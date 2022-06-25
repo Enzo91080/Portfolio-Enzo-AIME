53d8aecb6a6f3adbe81e38dd302a14a9c47bb546
Enzo91080 <76437510+EnzoSio@users.noreply.github.com>
2022-06-25 16:57:24 +0200

    Ajout Modal Blog

--- assets/js/script.js
+++ assets/js/script.js
@@ -167,3 +167,25 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
     });
 }
+
+
+function pause(ms) 
+{
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function afficherDate() 
+{
+  while(true) 
+  {
+    await pause(1000);
+    var cejour = new Date();
+    var options = {month: "long", day: "2-digit"};
+    var date = cejour.toLocaleDateString("fr-FR", options);
+    var heure = ("0" + cejour.getHours()).slice(-2) + ":" + ("0" + cejour.getMinutes()).slice(-2) + ":" + ("0" + cejour.getSeconds()).slice(-2);
+    var dateheure = date;
+    var dateheure = dateheure.replace(/(^\w{1})|(\s+\w{1})/g, lettre => lettre.toUpperCase());
+    document.getElementById('dateheure').innerHTML = dateheure;
+  }
+}
+afficherDate();
